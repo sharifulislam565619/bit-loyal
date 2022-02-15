59ff3b078bfdaa6d4384056d3dd11e7bbf7a2142
sharifulislam565619 <sharifulislam565619@gmail.com>
2022-02-15 23:38:45 +0600

    navigation bar added

---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navigation from '../Navigation/Navigation';
+
+const Home = () => {
+   return (
+      <div>
+         <Navigation></Navigation>
+      </div>
+   );
+};
+
+export default Home;
