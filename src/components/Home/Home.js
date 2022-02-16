763df2ced65bea3801e8973593ad7bcfa1bdce6d
sharifulislam565619 <sharifulislam565619@gmail.com>
2022-02-16 09:58:23 +0600

    background image add

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,10 +2,11 @@ import React from 'react';
 import About from '../About/About';
 import Banner from '../Banner/Banner';
 import Navigation from '../Navigation/Navigation';
+import './Home.css';
 
 const Home = () => {
    return (
-      <div>
+      <div id='home'>
          <Navigation></Navigation>
          <Banner></Banner>
          <About></About>
